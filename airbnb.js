a299e05d14e5493f87fc4af903c7c26e259f7965
Tanavv <70759298+Tanavv@users.noreply.github.com>
2020-09-11 21:40:39 +0800

    added new js

---
+++ airbnb.js
@@ -0,0 +1,9 @@
+function submitform(){
+	var where=document.getElementById('where').value;
+	var check_in=document.getElementById('Check-in').value;
+	var check_out=document.getElementById('Check-out').value;
+	var dropdown=document.getElementById('dropdown').value;
+	alert(where +" "+check_in + " "+check_out+" "+dropdown);
+
+
+}
