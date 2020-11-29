246bae3918e509edd193ff75f1fe3d13fd16fa80
Tanavv <70759298+Tanavv@users.noreply.github.com>
2020-11-29 21:00:02 +0800

    Update airbnb.js
    updated the javascript

--- airbnb.js
+++ airbnb.js
@@ -3,7 +3,15 @@ function submitform(){
 	var check_in=document.getElementById('Check-in').value;
 	var check_out=document.getElementById('Check-out').value;
 	var dropdown=document.getElementById('dropdown').value;
-	alert(where +" "+check_in + " "+check_out+" "+dropdown);
-
-
+	var map= {w:where, ci:check_in, check_out, d:dropdown};
+	var storage_data=localStorage.getItem("Travel")
+	if (storage_data===null)
+	{
+		var list=[]
+	} else
+	{
+		var list= JSON.parse(storage_data)
+	}
+	list.push(list)
+	localStorage.setItem("Travel", JSON.stringify(list))
 }
